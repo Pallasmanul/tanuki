f970fe659029d8ba2616e4f7aaf62569d41bf785
pallasmanul <pallasmanul@pallasmanul.>
2024-01-22 20:11:58 +0800

    add components and some

--- src/board/board.c
+++ src/board/board.c
@@ -6,6 +6,21 @@
 
 #include "board.h"
 
+static void log_uart_init(void) {
+  rcc_periph_clock_enable(RCC_USART1);
+
+  gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+                GPIO_USART1_TX);
+
+  usart_set_baudrate(LOG_USART, 115200);
+  usart_set_databits(USART1, 8);
+  usart_set_stopbits(USART1, USART_STOPBITS_1);
+  usart_set_mode(USART1, USART_MODE_TX_RX);
+  usart_set_parity(USART1, USART_PARITY_NONE);
+  usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+
+  usart_enable(USART1);
+}
 
 void board_init(void) {
 
@@ -16,18 +31,8 @@ void board_init(void) {
   rcc_periph_clock_enable(RCC_GPIOD);
   rcc_periph_clock_enable(RCC_GPIOE);
   rcc_periph_clock_enable(RCC_GPIOF);
-  rcc_periph_clock_enable(RCC_USART1);
 
-  gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
-
-  usart_set_baudrate(USART1, 115200);
-  usart_set_databits(USART1, 8);
-  usart_set_stopbits(USART1, USART_STOPBITS_1);
-  usart_set_mode(USART1, USART_MODE_TX_RX);
-  usart_set_parity(USART1, USART_PARITY_NONE);
-  usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-
-
+  log_uart_init();
 }
 
 
--- src/board/board.h
+++ src/board/board.h
@@ -1,6 +1,15 @@
 #ifndef TANUKI_BOARD_H
 #define TANUKI_BOARD_H
 
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/i2c.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/usart.h>
+
+//-- use uart default gpio pin
+#define LOG_USART USART1
+
 
 
 
